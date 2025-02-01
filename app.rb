43d6475a965cd51c03712bf36389596864cac1ba
vivianbarclay <vivian.m.barclay@gmail.com>
2025-02-01 15:46:47 +0000

    dynamic dice roll

--- app.rb
+++ app.rb
@@ -52,3 +52,19 @@ get("/dice/5/4") do
 
   erb(:five_four)
 end
+
+#Dynamic code
+get("/dice/:number_of_rolls/:number_of_sides") do
+  @rolls = []
+
+  number_of_rolls = params[:number_of_rolls].to_i
+  number_of_sides = params[:number_of_sides].to_i
+
+  number_of_rolls.times do
+    die = rand(1..number_of_sides)
+
+    @rolls.push(die)
+  end
+  
+  erb(:flexible)
+end  
