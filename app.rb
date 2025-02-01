e51fadd69138c22428b2fb170db76bea6bd8ba54
vivianbarclay <vivian.m.barclay@gmail.com>
2025-02-01 15:48:15 +0000

    deleted all other lines except for dynamic and home page

--- app.rb
+++ app.rb
@@ -5,54 +5,6 @@ get("/") do
   erb(:homepage)
 end
 
-get("/dice/2/6") do
-  @rolls = []
-
-  2.times do
-    die = rand(1..6)
-
-    @rolls.push(die)
-  end
-
-  erb(:two_six)
-end
-
-get("/dice/2/10") do
-  @rolls = []
-
-  2.times do
-    die = rand(1..10)
-
-    @rolls.push(die)
-  end
-
-  erb(:two_ten)
-end
-
-get("/dice/1/20") do
-  @rolls = []
-
-  1.times do
-    die = rand(1..20)
-
-    @rolls.push(die)
-  end
-
-  erb(:one_twenty)
-end
-
-get("/dice/5/4") do
-  @rolls = []
-
-  5.times do
-    die = rand(1..4)
-
-    @rolls.push(die)
-  end
-
-  erb(:five_four)
-end
-
 #Dynamic code
 get("/dice/:number_of_rolls/:number_of_sides") do
   @rolls = []
